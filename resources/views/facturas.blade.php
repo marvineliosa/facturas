5bd2e549a286a02b1fa93566aae27a245464614f
Marvin Eliosa <marvineliosa@gmail.com>
2019-07-30 16:21:17 -0500

    Se crea proyecto de laravel

---
+++ resources/views/facturas.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Facturas</title>
+	
+</head>
+<body>
+
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"/>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<div class="container">  
+    <div class="panel panel-default">
+        <div class="panel-heading"> SignUp to the system </div>
+        <div class="panel-body">
+            Test Panel
+        </div>
+        <div class="panel-footer"> You have 3 More attempts left! </div>
+    </div>
+</div>
+	
+</body>
+</html>
